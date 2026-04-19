0a306996ea22259311e7f7bc08d376e5a648e3c4
Alec Cheng <77299702+alec-cheng3@users.noreply.github.com>
2026-04-18 19:02:38 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,123 +1,76 @@
 #include "main.h"
 #include "usart.h"
+
 void SystemClock_Config(void);
+
 int main(void)
 {
-HAL_Init();
-SystemClock_Config();
-UART_init();
-char *str = "[3B";
-char *str2 = "[5C";
-UART_ESC_Code();
-UART_print(str);
-UART_ESC_Code();
-UART_print(str2);
-UART_print("All good students read the");
-UART_ESC_Code();
-UART_print("[1B");
-UART_ESC_Code();
-UART_print("[21D");
-UART_ESC_Code();
-UART_print("[5m");
-UART_print("Reference Manuel");
-UART_ESC_Code();
-UART_print("[H");
-UART_ESC_Code();
-UART_print("[0m");
-UART_print("Input: ");
-while (1)
+    HAL_Init();
+    SystemClock_Config();
+    UART_init();
+
+    char *str = "[3B";
+    char *str2 = "[5C";
+
+    UART_ESC_Code();
+    UART_print(str);
+
+    UART_ESC_Code();
+    UART_print(str2);
+
+    UART_print("All good students read the");
+
+    UART_ESC_Code();
+    UART_print("[1B");
+
+    UART_ESC_Code();
+    UART_print("[21D");
+
+    UART_ESC_Code();
+    UART_print("[5m");
+    UART_print("Reference Manuel");
+
+    UART_ESC_Code();
+    UART_print("[H");
+
+    UART_ESC_Code();
+    UART_print("[0m");
+    UART_print("Input: ");
+
+    while (1)
+    {
+    }
+}
+
+void USART2_IRQHandler(void)
 {
+    if (USART2->ISR & USART_ISR_RXNE)
+    {
+        char received_char = USART2->RDR;
+
+        if (received_char == 'R')
+        {
+            UART_ESC_Code();
+            UART_print("[31m"); // red
+        }
+        else if (received_char == 'B')
+        {
+            UART_ESC_Code();
+            UART_print("[36m"); // blue/cyan in ANSI
+        }
+        else if (received_char == 'G')
+        {
+            UART_ESC_Code();
+            UART_print("[32m"); // green
+        }
+        else if (received_char == 'W')
+        {
+            UART_ESC_Code();
+            UART_print("[37m"); // white
+        }
+        else
+        {
+            USART2->TDR = received_char;
+        }
+    }
 }
-}
-void USART2_IRQHandler(void) {
-if (USART2->ISR & USART_ISR_RXNE) {
-char received_char = USART2->RDR;
-if (received_char == 'R') {
-UART_ESC_Code();
-UART_print("[31m"); // change text color to red
-} else if (received_char == 'B') {
-UART_ESC_Code();
-UART_print("[36m"); // change text color to blue
-} else if (received_char == 'G') {
-UART_ESC_Code();
-UART_print("[32m"); // change text color to green
-} else if (received_char == 'W') {
-UART_ESC_Code();
-UART_print("[37m"); // change text color to white
-} else {
-// echo received character back to terminal
-USART2->TDR = received_char;
-}
-}
-}
-/**
-* @brief System Clock Configuration
-* @retval None
-*/
-void SystemClock_Config(void)
-{
-RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-/** Configure the main internal regulator output voltage
-*/
-if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-{
-Error_Handler();
-}
-/** Initializes the RCC Oscillators according to the specified parameters
-* in the RCC_OscInitTypeDef structure.
-*/
-RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
-RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-RCC_OscInitStruct.MSICalibrationValue = 0;
-RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-{
-Error_Handler();
-}
-/** Initializes the CPU, AHB and APB buses clocks
-*/
-RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
-RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-{
-Error_Handler();
-}
-}
-/* USER CODE BEGIN 4 */
-/* USER CODE END 4 */
-/**
-* @brief This function is executed in case of error occurrence.
-* @retval None
-*/
-void Error_Handler(void)
-{
-/* USER CODE BEGIN Error_Handler_Debug */
-/* User can add his own implementation to report the HAL error return state */
-__disable_irq();
-while (1)
-{
-}
-/* USER CODE END Error_Handler_Debug */
-}
-#ifdef USE_FULL_ASSERT
-/**
-* @brief Reports the name of the source file and the source line number
-* where the assert_param error has occurred.
-* @param file: pointer to the source file name
-* @param line: assert_param error line source number
-* @retval None
-*/
-void assert_failed(uint8_t *file, uint32_t line)
-{
-/* USER CODE BEGIN 6 */
-/* User can add his own implementation to report the file name and line number,
-ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-/* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
